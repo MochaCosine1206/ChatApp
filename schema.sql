51d0da9074c6730657f85e7f19717368ff63871b
StevenXPS13 <steven.parrott@gmail.com>
2019-02-26 20:18:42 -0700

    we have sign in

--- schema.sql
+++ schema.sql
@@ -1,1 +1,4 @@
-CREATE DB chatapp;
+-- Drops the blogger if it exists currently --
+DROP DATABASE IF EXISTS chatapp;
+-- Creates the "blogger" database --
+CREATE DATABASE chatapp;
--- server.js
+++ server.js
@@ -5,32 +5,37 @@
 // *** Dependencies
 // =============================================================
 var express = require("express");
+var session = require("express-session");
+var passport = require("./config/passport");
 
 // Sets up the Express App
 // =============================================================
-var app = express();
+
 var PORT = process.env.PORT || 8080;
 
 // Requiring our models for syncing
 var db = require("./models");
-
+var app = express();
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Static directory
 app.use(express.static("public"));
+app.use(session({ secret: "keyboard cat", resave: true, saveUninitialized: true }));
+app.use(passport.initialize());
+app.use(passport.session());
 
 // Routes
 // =============================================================
-require("./routes/html-routes.js")(app);
-require("./routes/author-api-routes.js")(app);
-require("./routes/post-api-routes.js")(app);
+require("./routes/html-routes-api.js")(app);
+require("./routes/register-api-routes.js")(app);
+
 
 // Syncing our sequelize models and then starting our Express app
 // =============================================================
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
-    console.log("App listening on PORT " + PORT);
+    console.log("==> 🌎  Listening on port %s. Visit http://localhost:%s/ in your browser.", PORT, PORT);
   });
 });
