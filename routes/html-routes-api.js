6af8cdbd7d024b460e6bbadf04526c667039a9ae
Patrycja Gorczynska <patrycjagorczynska@Patrycjas-MacBook-Pro.local>
2019-02-28 19:56:24 -0700

    made home page

--- routes/html-routes-api.js
+++ routes/html-routes-api.js
@@ -9,7 +9,15 @@ module.exports = function(app) {
   app.get("/", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/chats");
+    }
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
+
+  app.get("/signup", function(req, res) {
+    // If the user already has an account send them to the members page
+    if (req.user) {
+      res.redirect("/chats");
     }
     res.sendFile(path.join(__dirname, "../public/signup.html"));
   });
@@ -17,7 +25,7 @@ module.exports = function(app) {
   app.get("/login", function(req, res) {
     // If the user already has an account send them to the members page
     if (req.user) {
-      res.redirect("/members");
+      res.redirect("/chats");
     }
     res.sendFile(path.join(__dirname, "../public/login.html"));
   });
