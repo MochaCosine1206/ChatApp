f36e532ee485fd58a9dd20615893ac20bde814d0
Xybolx <xybolx@gmail.com>
2019-03-02 13:35:01 -0700

    regex for login and userprofile input added

--- public/JS/userprofile.js
+++ public/JS/userprofile.js
@@ -86,6 +86,50 @@ $(document).ready(function () {
         $("#alert").fadeIn(500);
       }
 
+    // Client-side validation
+      $('#userprofile-submit').prop('disabled', true);
+      $('#name').on('input', function () {
+        var input = $(this);
+        var re =/^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/;
+        var is_name = re.test(input.val());
+        if (is_name) {
+          input.removeClass("invalid").addClass("valid");
+          $('#userprofile-submit').prop('disabled', true);
+        }
+        else {
+          input.removeClass("valid").addClass("invalid");
+          $('#signup-submit').prop('disabled', true);
+        }
+    });
+
+    $('#userName').on('input', function () {
+        var input = $(this);
+        var re = /^[A-Za-z 0-9 (!?@#$%&)]{3,15}$/;
+        var is_userName = re.test(input.val());
+        if (is_userName) {
+          input.removeClass("invalid").addClass("valid");
+          $('#userprofile-submit').prop('disabled', true);
+        }
+        else {
+          input.removeClass("valid").addClass("invalid");
+          $('#userprofile-submit').prop('disabled', true);
+        }
+    });
+
+    $('#userAboutMe').on('input', function () {
+        var input = $(this);
+        var re = /^[ a-zA-Z0-9_][a-zA-Z0-9_ ]*[a-zA-Z0-9_]$/;
+        var is_about = re.test(input.val());
+        if (is_about) {
+          input.removeClass("invalid").addClass("valid");
+          $('#userprofile-submit').prop('disabled', false);
+        }
+        else {
+          input.removeClass("valid").addClass("invalid");
+          $('#userprofile-submit').prop('disabled', true);
+        }
+    });
+
         
 
 });
