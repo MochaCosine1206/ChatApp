3d69724c8b15ee8ec2a792a7d9979aa2be5ac321
Xybolx <xybolx@gmail.com>
2019-03-02 15:08:54 -0700

    client validation for userprofile inputs

--- public/JS/userprofile.js
+++ public/JS/userprofile.js
@@ -90,18 +90,23 @@ $(document).ready(function () {
       }
 
     // Client-side validation
+      $('#nameInvalid').hide();
+      $('#userNameInvalid').hide();
+      $('#aboutInvalid').hide();
       $('#userprofile-submit').prop('disabled', true);
       $('#name').on('input', function () {
         var input = $(this);
-        var re =/^[a-zA-Z][a-zA-Z ]+[a-zA-Z]$/;
+        var re =/^[a-zA-Z][a-zA-Z ]+[a-zA-Z]{3,15}$/;
         var is_name = re.test(input.val());
         if (is_name) {
           input.removeClass("invalid").addClass("valid");
           $('#userprofile-submit').prop('disabled', true);
+          $('#nameInvalid').hide();
         }
         else {
           input.removeClass("valid").addClass("invalid");
-          $('#signup-submit').prop('disabled', true);
+          $('#userprofile-submit').prop('disabled', true);
+          $('#nameInvalid').show();
         }
     });
 
@@ -112,10 +117,12 @@ $(document).ready(function () {
         if (is_userName) {
           input.removeClass("invalid").addClass("valid");
           $('#userprofile-submit').prop('disabled', true);
+          $('#userNameInvalid').hide();
         }
         else {
           input.removeClass("valid").addClass("invalid");
           $('#userprofile-submit').prop('disabled', true);
+          $('#userNameInvalid').show();
         }
     });
 
@@ -126,10 +133,12 @@ $(document).ready(function () {
         if (is_about) {
           input.removeClass("invalid").addClass("valid");
           $('#userprofile-submit').prop('disabled', false);
+          $('#aboutInvalid').hide();
         }
         else {
           input.removeClass("valid").addClass("invalid");
           $('#userprofile-submit').prop('disabled', true);
+          $('#aboutInvalid').show();
         }
     });
 
