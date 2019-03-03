7c4486747f6d62543518d207503ca2cedc54874d
MochaCosine1206 <steven.parrott@gmail.com>
2019-03-03 15:00:16 -0700

    now multiple contacts per USERID

--- models/contacts.js
+++ models/contacts.js
@@ -1,14 +1,11 @@
 module.exports = function(sequelize, DataTypes) {
     var Contacts = sequelize.define("Contacts", {
+      ContactId: DataTypes.UUID
     });
   
     Contacts.associate = function(models) {
       // Many Contacts can belong to a userProfile but can't be created unless an userProfile is created first
-      Contacts.belongsTo(models.UserProfile, {
-        foreignKey: {
-          allowNull: false
-        }
-      });
+      Contacts.belongsTo(models.UserProfile);
     };
   
     return Contacts;
--- public/JS/chats.js
+++ public/JS/chats.js
@@ -1,9 +1,9 @@
 $(document).ready(function () {
 
-        let socket = io.connect();
+    let socket = io.connect();
 
 
-        //below is socket demo for future reference
+    //below is socket demo for future reference
 
     // nameInput.keyup(function(){
     //     socket.emit('send profile', nameInput.val());
@@ -16,10 +16,18 @@ $(document).ready(function () {
     chatStart = $("#chatStart");
     contactSearch = $("#contactSearch")
     let contacts;
+    let currentUser = "";
 
-    $("#chatbutton").click(function(){
+
+    $.get("/api/user_data").then(function (data) {
+        currentUser = data.id
+    });
+
+    $("#chatbutton").click(function () {
         chatStart.toggle();
     })
+
+
 
 
 
@@ -34,98 +42,100 @@ $(document).ready(function () {
         } else {
             initializeRows(contacts);
         }
+    });
+
+
+    function initializeRows(contacts) {
+        let searchphrase = "";
+        contactSearch.keyup(function () {
+            searchphrase = (contactSearch.val())
+        })
+
+
+        contactsCard.empty();
+        var contactsToAdd = [];
+        for (var i = 0; i < contacts.length; i++) {
+            contactsToAdd.push(createNewRow(contacts[i]));
+            console.log(contacts[i]);
+            console.log(contactsToAdd);
+        }
+        contactsCard.append(contactsToAdd);
+
+        contactSelect();
+    }
+
+    function createNewRow(contact) {
+        console.log("inside CreateNewRow: " + contact)
+        const newContactCard = $("<div>");
+        newContactCard.attr("class", "contactCard");
+        newContactCard.attr("addedToChat", "no");
+        newContactCard.attr("contactID", contact.id);
+        newContactCard.attr("contactUser", contact.userName);
+        newContactCard.data("contact", contact);
+        const newContactCardText = $("<div>");
+        newContactCardText.css({
+            marginTop: "10px"
+        })
+        newContactCard.css({
+            borderTop: "solid 1px grey",
+            height: "70px",
+            backgroundColor: "white",
+            padding: "3px",
         });
+        const newContactCardAvatar = $("<img>").attr("src", contact.avatar_seed);
+        const newContactCardTagline = $("<div>").text(contact.tagline);
+        newContactCardTagline.css({
+            fontSize: "12px"
+        })
+        newContactCardAvatar.addClass("img-thumbnail");
+        newContactCardAvatar.css({
+            height: "60px",
+            float: "left",
+            marginRight: "3px"
+        })
+        // var newContactCardConnected use this when created.  When user connects on socket, status will change.
+        newContactCardText.text(" " + contact.name + " | " + contact.userName);
+        newContactCard.append(newContactCardAvatar);
+        newContactCard.append(newContactCardText, newContactCardTagline);
+        return newContactCard;
+    }
 
-
-        function initializeRows(contacts) {
-            let searchphrase = "";
-            contactSearch.keyup(function(){
-                searchphrase = (contactSearch.val())
-            })
-
-
-            contactsCard.empty();
-            var contactsToAdd = [];
-            for (var i = 0; i < contacts.length; i++) {
-                contactsToAdd.push(createNewRow(contacts[i]));
-                console.log(contacts[i]);
-                console.log(contactsToAdd);
-            }
-            contactsCard.append(contactsToAdd);
-
-            contactSelect();
-        }
-
-        function createNewRow(contact) {
-            console.log("inside CreateNewRow: " + contact)
-            const newContactCard = $("<div>");
-            newContactCard.attr("class", "contactCard");
-            newContactCard.attr("addedToChat", "no");
-            newContactCard.attr("contactID", contact.id);
-            newContactCard.attr("contactUser", contact.userName);
-            newContactCard.data("contact", contact);
-            const newContactCardText = $("<div>");
-            newContactCardText.css({
-                marginTop: "10px"
-            })
-            newContactCard.css({
-                borderTop: "solid 1px grey",
-                height: "70px",
-                backgroundColor: "white",
-                padding: "3px",
-            });
-            const newContactCardAvatar = $("<img>").attr("src", contact.avatar_seed);
-            const newContactCardTagline = $("<div>").text(contact.tagline);
-            newContactCardTagline.css({
-                fontSize: "12px"
-            })
-            newContactCardAvatar.addClass("img-thumbnail");
-            newContactCardAvatar.css({
-                height: "60px",
-                float: "left",
-                marginRight: "3px"
-            })
-            // var newContactCardConnected use this when created.  When user connects on socket, status will change.
-            newContactCardText.text(" " + contact.name + " | " + contact.userName);
-            newContactCard.append(newContactCardAvatar);
-            newContactCard.append(newContactCardText, newContactCardTagline);
-            return newContactCard;
-        }
-
-        function contactSelect(){
-            let contactCard = $('.contactCard');
-            let contactSearch = $('#contactSearch');
-            let contactPickButton = $('<button>');
-            contactPickButton.addClass("btn mr-3");
-            contactPickButton.css({
-                backgroundColor: "lightblue"
-            });
-            contactCard.on("click",function(){
-                let contactState = $(this).attr('addedToChat');
-                let contactStateId = $(this).attr('contactID');
-                let contactStateUser = $(this).attr('contactUser');
-                if (contactState === 'no'){
-                    $(this).css({
-                        backgroundColor: "lightblue",
-                    }).attr('addedToChat', 'yes');
-                    contactPickButton.attr("contactId", contactStateId)
-                    contactSearch.val(contactStateUser);
-                    var newContact = {
-                        UserProfileId : contactStateId
-                    }
-                    submitContact(newContact);
-                } else {
-                    $(this).css({
-                        backgroundColor: "white",
-                    }).attr('addedToChat', 'no');
-                    contactPickButton.attr("contactId", contactStateId)
+    function contactSelect() {
+        let contactCard = $('.contactCard');
+        let contactSearch = $('#contactSearch');
+        let contactPickButton = $('<button>');
+        contactPickButton.addClass("btn mr-3");
+        contactPickButton.css({
+            backgroundColor: "lightblue"
+        });
+        contactCard.on("click", function () {
+            let contactState = $(this).attr('addedToChat');
+            let contactStateId = $(this).attr('contactID');
+            let contactStateUser = $(this).attr('contactUser');
+            if (contactState === 'no') {
+                $(this).css({
+                    backgroundColor: "lightblue",
+                }).attr('addedToChat', 'yes');
+                contactPickButton.attr("contactId", contactStateId)
+                contactSearch.val(contactStateUser);
+                var newContact = {
+                    ContactId: currentUser,
+                    UserProfileId: contactStateId
+                    
                 }
-            })
-        }
+                submitContact(newContact);
+            } else {
+                $(this).css({
+                    backgroundColor: "white",
+                }).attr('addedToChat', 'no');
+                contactPickButton.attr("contactId", contactStateId)
+            }
+        })
+    }
 
-        function submitContact(newContact) {
-            $.post("api/contacts", newContact)
-        }
+    function submitContact(newContact) {
+        $.post("api/contacts", newContact)
+    }
 
 
 });
